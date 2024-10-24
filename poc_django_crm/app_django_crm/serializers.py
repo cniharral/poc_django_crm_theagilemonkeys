1af3f428e32ab35198597e8fd93c1ca7a0b487af
CNL <git@zynetyka.com>
2024-10-24 23:03:17 +0200

    Added fields in Meta class of CustomerSerializer.

--- poc_django_crm/app_django_crm/serializers.py
+++ poc_django_crm/app_django_crm/serializers.py
@@ -4,4 +4,5 @@ from app_django_crm.models import Customer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
+        fields = ['id', 'name', 'surname', 'photo', 'last_creation_userid', 'last_update_userid']
         exclude = []
