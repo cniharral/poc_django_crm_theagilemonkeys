aa46807d8ee916c07f83fe10ecb29403144e0863
CNL <git@zynetyka.com>
2024-10-24 22:59:19 +0200

    Added static and media files config.

--- poc_django_crm/poc_django_crm/urls.py
+++ poc_django_crm/poc_django_crm/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from app_django_crm.views import *
 from app_django_crm import views
 
@@ -26,3 +28,8 @@ urlpatterns = [
     path('v1/customer/', Customer_APIView.as_view()),
     path('v1/customer/<int:pk>/', Customer_APIView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
