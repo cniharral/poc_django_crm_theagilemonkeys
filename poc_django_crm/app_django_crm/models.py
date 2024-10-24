4394de529b032581d62588bc271378f3a0a4afc0
CNL <git@zynetyka.com>
2024-10-24 23:01:27 +0200

    Changed 'upload_to' value of photo field in the model.

--- poc_django_crm/app_django_crm/models.py
+++ poc_django_crm/app_django_crm/models.py
@@ -6,7 +6,7 @@ from drf_extra_fields.fields import Base64ImageField
 class Customer(models.Model):
     name = models.CharField(max_length=80)
     surname = models.CharField(max_length=80)
-    photo = models.ImageField(upload_to="media/", height_field=None, width_field=None, max_length=100, null=True)
+    photo = models.ImageField(upload_to="images/", height_field=None, width_field=None, max_length=100, null=True)
     #photo = model.Base64ImageField(required=False)
     last_creation_userid = models.IntegerField(null=True)
     last_update_userid = models.IntegerField(null=True)
