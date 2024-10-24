30d5370dc4feb81f54075211707c7ca6644482ec
CNL <git@zynetyka.com>
2024-10-24 23:45:53 +0200

    Removing deprecated contents.

--- poc_django_crm/app_django_crm/views.py
+++ poc_django_crm/app_django_crm/views.py
@@ -8,8 +8,6 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .models import Customer
 from .serializers import CustomerSerializer
-from django.http import Http404
-from django.shortcuts import get_object_or_404
 
 class Customer_APIView(APIView):
     authentication_classes = [TokenAuthentication]
@@ -20,12 +18,14 @@ class Customer_APIView(APIView):
         serializer = CustomerSerializer(customers, many=True)
 
         return Response(serializer.data)
+
     def post(self, request, format=None):
         serializer = CustomerSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def put(self, request, pk, format=None):
         customer = Customer.objects.get(pk=pk)
         serializer = CustomerSerializer(customer, data=request.data)
@@ -33,7 +33,9 @@ class Customer_APIView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk, format=None):
         customer = Customer.objects.get(pk=pk)
         customer.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
